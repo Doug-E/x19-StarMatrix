f9b627f65a11c48d1c9dd2978bc20b91716c0453
Doug-E <dmullaly@mail.csuchico.edu>
2017-10-18 17:55:23 -0700

    Working star program

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+        vector <vector <char> > starBro;
+        int row, col;
+
+        cout<< "How many rows and columns would you like?" <<endl;
+        cin>> row;
+        
+        if (row < 1) return 0;
+
+        cin>> col;
+
+        if (col < 1) return 0;
+
+        starBro.resize(row);
+
+        for (int r = 0; r<starBro.size(); r++)
+        {
+                starBro[r].resize(col);
+                for (int c = 0; c < starBro[r].size(); c++)
+                {
+                        starBro[r][c] = '*';
+                        cout<< starBro[r][c];
+                }
+                cout<<endl;
+        }
+        return 0;
+}
